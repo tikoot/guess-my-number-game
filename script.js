7e56f000b7486e2b04c0bd9f155f5f4307d037db
tikoot <tiko.otarashvili99@gmail.com>
2023-03-27 10:09:52 +0400

    initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+
+const displayMessage = (message) => {
+    document.querySelector('.message').textContent = message;
+}
+
+document.querySelector('.check').addEventListener('click', function() {
+
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess,typeof guess);
+
+    if (!guess) {
+        displayMessage('⛔ No number!')
+    }else if (guess == secretNumber){
+         displayMessage('🎉 Correct Number!');
+         document.querySelector('body').style.backgroundColor = '#60b347';
+         document.querySelector('.number').textContent = secretNumber;
+
+
+         if(score > highScore){
+            highScore = score;
+            document.querySelector('.highscore').textContent = highScore;
+         }
+    }else if(guess !== secretNumber){
+          if(score > 1) {
+            displayMessage(guess > secretNumber ? 'Too high!' : 'Too low!');
+             score--;
+             document.querySelector('.score').textContent = score;
+        }else{
+            displayMessage('You lost the game!');
+            document.querySelector('.score').textContent = 0;
+        }
+    } 
+});
+
+document.querySelector('.again').addEventListener('click', function() {
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+    displayMessage('Start guessing...');
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+    document.querySelector('body').style.backgroundColor = '#222';
+
+})
